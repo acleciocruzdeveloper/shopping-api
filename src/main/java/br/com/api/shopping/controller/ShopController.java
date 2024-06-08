9f8d75a3406dc463f470bd706e503528584e16be
acleciocruz <aclecioscruz@gmail.com>
2024-06-08 20:22:06 -0300

    method find by user identifier shop controller

--- src/main/java/br/com/api/shopping/controller/ShopController.java
+++ src/main/java/br/com/api/shopping/controller/ShopController.java
@@ -7,10 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -23,5 +25,11 @@ public class ShopController {
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<ShopDto>> getShops() {
         return ResponseEntity.ok().body(shopService.getAll());
+    }
+
+    @GetMapping("/shopping/shopByUser/{userIdentifier}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<ShopDto>> getShops(@PathVariable String userIdentifier) {
+        return ResponseEntity.ok().body(shopService.getByUser(userIdentifier));
     }
 }
